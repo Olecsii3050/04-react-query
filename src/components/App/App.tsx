2a08dd4e31f7a6d69a69b00856630aa150b2b79b
Olecsii <vyriy3050@gmail.com>
2025-07-24 00:32:34 +0300

    04-react-query

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,26 +5,34 @@ import MovieGrid from "../../components/MovieGrid/MovieGrid";
 import Loader from "../../components/Loader/Loader";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import MovieModal from "../../components/MovieModal/MovieModal";
+import ReactPaginate from "react-paginate";
 import type { Movie } from "../../types/movie";
 import toast, { Toaster } from "react-hot-toast";
+import css from "./App.module.css";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearch = async (query: string) => {
+  const handleSearch = async (query: string, page: number = 1) => {
     setLoading(true);
     setError("");
+    setSearchQuery(query);
     try {
-      const results = await fetchMovies(query);
+      const results: Movie[] = await fetchMovies(query, page);
       setMovies(results);
+      setTotalPages(Math.ceil(results.length / 2));
       if (results.length === 0) {
         toast.error("No movies found for your request.");
       }
-    } catch {
+    } catch (error) {
       setError("There was an error fetching movies.");
+      console.error(error);
     } finally {
       setLoading(false);
     }
@@ -38,11 +46,29 @@ function App() {
     setSelectedMovie(null);
   };
 
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    setPage(selected + 1);
+    handleSearch(searchQuery, selected + 1);
+  };
+
   return (
     <div>
       <SearchBar onSubmit={handleSearch} />
       {loading && <Loader />}
       {error && <ErrorMessage />}
+      {totalPages > 1 && (
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={handlePageChange}
+          forcePage={page - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
+      )}
       <MovieGrid movies={movies} onSelect={handleSelectMovie} />
       {selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -7,9 +7,13 @@ const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 interface MovieResponse {
   results: Movie[];
+  total_pages: number;
 }
 
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (
+  query: string,
+  page: number = 1
+): Promise<Movie[]> => {
   const response: AxiosResponse<MovieResponse> = await axios.get<MovieResponse>(
     API_URL,
     {
@@ -17,7 +21,7 @@ export const fetchMovies = async (query: string): Promise<Movie[]> => {
         query,
         include_adult: false,
         language: "en-US",
-        page: 1,
+        page,
       },
       headers: {
         Authorization: `Bearer ${TOKEN}`,
